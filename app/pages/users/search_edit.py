b5699a740e2974a34768cb24f47d38f2ee2d20b0
Mafil-Solutions <cohen_m@mafil.co.il>
2025-11-02 01:37:55 +0200

    Update search_edit.py

--- app/pages/users/search_edit.py
+++ app/pages/users/search_edit.py
@@ -104,7 +104,7 @@ def show():
     st.subheader("חיפוש")
 
     # שורה ראשונה: מקור (בצד ימין)
-    col_spacer, col_provider = st.columns([1, 1])
+    col_spacer, col_provider = st.columns([5, 1])
     with col_spacer:
         pass  # עמודה ריקה משמאל
     with col_provider:
