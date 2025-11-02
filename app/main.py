504dbfab70d0cb9d60fb7a9919f9742a86978a26
Mafil-Solutions <cohen_m@mafil.co.il>
2025-11-02 03:41:44 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -354,7 +354,7 @@ def show_compact_user_info():
             height: 1.5rem !important;
             min-height: 1.5rem !important;
             background: #f8f9fa!important;
-            color: #f8f9fa !important;
+            color: #666 !important;
             border: 1px solid #ddd !important;
             border-radius: 0.3rem !important;
         }
