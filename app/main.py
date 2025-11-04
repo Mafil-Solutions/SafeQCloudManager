022575e0d794ee4ee45b797611301494869780b7
Mafil-Solutions <cohen_m@mafil.co.il>
2025-11-04 14:21:29 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -97,16 +97,7 @@ def apply_modern_styling_compact(rtl=False):
             transform: rotate(180deg) !important;
 }}
 
-        /* 2. עיצוב החץ במצב פתוח (כשהקטגוריה פתוחה)*/
-        [data-testid="stSidebar"] details[open] summary [data-testid="stIconMaterial"] {{
-            /* ✨ פקודת הסיבוב: הופך את החץ למעלה */
-            transform: rotate(270deg) !important; 
-            /* מוודא שהנראות נשמרת גם במצב פתוח */
-            visibility: visible !important;
-            opacity: 1 !important;
-            transition: none !important;
-            color: gainsboro;
-        }}
+       
         [data-testid="stSidebar"] details[open] summary span[data-testid="stIconMaterial"],
         [data-testid="stSidebar"] details[open] summary div span[data-testid="stIconMaterial"] {{
             color: #333 !important;
