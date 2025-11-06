08f8786c42bbf4fb0e142660cf99bf2836e95cee
Mafil-Solutions <cohen_m@mafil.co.il>
2025-11-06 17:59:57 +0200

    Update add_user.py

--- app/pages/users/add_user.py
+++ app/pages/users/add_user.py
@@ -89,7 +89,7 @@ def show():
             border: none !important;
         }
 
-        button[data-testid="stBaseButton-primaryFormSubmit"] {:hover {
+        button[data-testid="stBaseButton-primaryFormSubmit"] :hover {
             background-color: linear-gradient(45deg, #FF6B6B, #C41E3A) !important;
             color: white !important;
         }
